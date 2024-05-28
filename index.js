651bf078c44deccf0ba74f17a6af94d2ce4be75a
unknown <muizzuddin334@gmail.com>
2024-05-28 12:16:13 +0700

    fix env

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import app from "./app.js";
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is running on http://localhost:${PORT}`)
+);
